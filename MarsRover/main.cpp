575fc5941c0650dfda19c1fe66dc9765035acc46
Bernes <56940210+Bernes99@users.noreply.github.com>
2021-04-26 20:14:20 +0200

    Revert "test"
    This reverts commit 3036cd8f35725be597a45487eca933c4c3f8c01c.

--- MarsRover/main.cpp
+++ MarsRover/main.cpp
@@ -25,7 +25,7 @@ GLdouble centerZ = -10;
 
 Rover rover(0, 0, -20, 0, 0, 0, 20);
 
-
+//TODO Add Point class maybe to clean the x y z mess up a little bit
 
 void drawBox(void)
 {
@@ -51,13 +51,7 @@ void display(void)
     glRotatef(rotZ, 0, 0,1);
 
 //    drawBox();
-    wheel.draw();
-    wheel2.draw();
-    wheel3.draw();
-    wheel4.draw();
-    wheel5.draw();
-    wheel6.draw();
-
+    rover.draw();
 
     // Flush drawing commands
     glFlush();
