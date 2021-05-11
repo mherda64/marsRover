b5eee10917a6cb6ca93e4b30288a24e17f405617
Musiek71 <Mzjedz@gmail.com>
2021-05-11 11:23:52 +0200

    Remove old project files.

--- MarsRover/BasicStructures/Point.cpp
+++
@@ -1,7 +0,0 @@
-//
-// Created by musiek on 26.04.2021.
-//
-
-#include "Point.h"
-
-Point::Point(GLdouble x, GLdouble y, GLdouble z) : x(x), y(y), z(z) {}
--- MarsRover/BasicStructures/Rotation.cpp
+++
@@ -1,7 +0,0 @@
-//
-// Created by musiek on 26.04.2021.
-//
-
-#include "Rotation.h"
-
-Rotation::Rotation(GLdouble xRot, GLdouble yRot, GLdouble zRot) : xRot(xRot), yRot(yRot), zRot(zRot) {}
